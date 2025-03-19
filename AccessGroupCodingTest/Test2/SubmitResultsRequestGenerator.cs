357a71101bf904a2a250ffe638e26c3edd8c7d55
Amry <shamsulamry@outlook.com>
2025-03-19 14:23:20 +0800

    perf: Cache subtext output

--- AccessGroupCodingTest/Test2/SubmitResultsRequestGenerator.cs
+++ AccessGroupCodingTest/Test2/SubmitResultsRequestGenerator.cs
@@ -9,13 +9,20 @@ public class SubmitResultsRequestGenerator : ISubmitResultsRequestGenerator
 {
     public SubmitResultsRequest GenerateRequest(TextToSearchResponse textToSearch, SubTextsResponse subTexts)
     {
+        var outputCache = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+
         var requestItems = new List<SubmitResultsRequestItem>();
         foreach (var subText in subTexts.SubTexts)
         {
-            var output = string.Join(", ", SubTextFinder.Find(textToSearch.Text, subText));
-            if (output == "")
+            if (!outputCache.TryGetValue(subText, out var output))
             {
-                output = SubmitResultsRequestItem.NoMatch;
+                output = string.Join(", ", SubTextFinder.Find(textToSearch.Text, subText));
+                if (output == "")
+                {
+                    output = SubmitResultsRequestItem.NoMatch;
+                }
+
+                outputCache.Add(subText, output);
             }
 
             requestItems.Add(new(subText, output));
